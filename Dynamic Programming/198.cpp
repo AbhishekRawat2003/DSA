9ee539c849fe20187b9b8b75f82cc8ff61200afa
Abhishek Rawat <abhirawathdr@gmail.com>
2026-07-03 10:06:15 +0530

    .

---
+++ Dynamic Programming/198.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+  int rob(vector<int> &nums) {
+    int n = nums.size();
+
+    if (n == 0)
+      return 0;
+    if (n == 1)
+      return nums[0];
+
+    vector<int> dp(n + 1);
+    dp[0] = nums[0];
+    dp[1] = max(nums[1], nums[0]);
+
+    for (int i = 2; i < n; i++) {
+      dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+    };
+    return dp[n - 1];
+  }
+};
+
+int main() {
+  vector<int> nums = {1, 2, 3, 1};
+  Solution s;
+  int res = s.rob(nums);
+  cout << res << endl;
+  return 0;
+}
